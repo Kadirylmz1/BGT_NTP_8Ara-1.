5568dfeb7f5562f0665e1741486c12d131126eb6
Lab1-1 <Lab1-1@DESKTOP-NHSN8JG>
2023-12-08 09:19:01 +0300

    Proje dosyası ekle.

---
+++ BGT_NTP_8Ara-1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_NTP_8Ara_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.AD = "Ahmet";
+            label1.Text = ogr1.AD;
+            label2.Text = ogr1.OKULNO.ToString();
+            ogr1.tarihNe();
+        }
+    }
+}
---
+++ BGT_NTP_8Ara-1/Ogrenci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_NTP_8Ara_1
+{
+    class Ogrenci
+    {
+        string ad;
+        int okulNo;
+
+        public string AD
+        {
+            get { 
+                return ad; 
+            } 
+            set { 
+                ad = value;
+            }
+        }
+
+        public int OKULNO
+        {
+            get { 
+                return okulNo;
+            }
+        }
+
+        public void tarihNe() {
+            MessageBox.Show("8Aralık2023");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
